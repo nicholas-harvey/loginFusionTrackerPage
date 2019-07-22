2688ae97cea3f925dea2603e6618d72a4a0e2062
unknown <nicholas.harvey1914@gmail.com>
2019-07-22 15:19:50 -0400

    functioning routes added using npm package react-router-dom

--- login-page/src/App.jsx
+++ login-page/src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import {LoginForm} from "./components/Login/LoginForm"
-import {FusionNavBar} from './components/NavBar/FusionNavBar'
+import {FusionNavBar} from './Components/NavBar/NavBar'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {TestPage} from "./Components/Test/Test"
+import {AndAnotherOne} from "./Components/AnotherOne/AnotherOne"
+import {AnothaPage} from "./Components/AndAnotha/AndAnotha"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <FusionNavBar />
-      <header className="App-header">
-        <LoginForm />
-      </header>
-    </div>
+    <div>
+ <Router>
+ <FusionNavBar />
+   <Route path="/logon" component={TestPage}/>
+   <Route path="/anotha" component={AndAnotherOne}/>
+   <Route path="/andanotha" component={AnothaPage}/>
+ </Router>
+
+ </div> 
   );
-}
+  };
 
 export default App;
--- login-page/src/components/NavBar/NavBar.jsx
+++ login-page/src/components/NavBar/NavBar.jsx
@@ -7,12 +7,12 @@ export class FusionNavBar extends Component {
         return(
           <div className="nav-container">
             <Navbar variant="dark">
-    <Navbar.Brand href="?">FusionTracker</Navbar.Brand>
+    <Navbar.Brand href="/">FusionTracker</Navbar.Brand>
     <Nav className="mr-auto">
-    <Nav.Link href="?">Home</Nav.Link>
-      <Nav.Link href="?">Login/Logout</Nav.Link>
-      <Nav.Link href="?">Pricing</Nav.Link>
-      <Nav.Link href="?">Preferences</Nav.Link>
+    <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/logon">Login/Logout</Nav.Link>
+      <Nav.Link href="/anotha">Pricing</Nav.Link>
+      <Nav.Link href="andanotha">Preferences</Nav.Link>
     </Nav>
   </Navbar>
   </div>
