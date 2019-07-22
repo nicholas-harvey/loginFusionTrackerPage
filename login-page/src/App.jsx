0ded073906edddb626af0c12f91e99a329c89b49
unknown <nicholas.harvey1914@gmail.com>
2019-07-22 17:41:11 -0400

    removed everything but app.jsx and loginform.jsx as this will be built here and then added to the fusion-tracker repo locally while the react-router-dom issue is resolved

--- login-page/src/App.jsx
+++ login-page/src/App.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import {FusionNavBar} from './Components/NavBar/NavBar'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import {TestPage} from "./Components/Test/Test"
-import {AndAnotherOne} from "./Components/AnotherOne/AnotherOne"
-import {AnothaPage} from "./Components/AndAnotha/AndAnotha"
+import {LoginForm} from "./Components/Login/Login";
 import './App.css';
 
 function App() {
   return (
     <div>
- <Router>
- <FusionNavBar />
-   <Route path="/logon" component={TestPage}/>
-   <Route path="/anotha" component={AndAnotherOne}/>
-   <Route path="/andanotha" component={AnothaPage}/>
- </Router>
-
+<LoginForm />
  </div> 
   );
   };
--- login-page/src/Components/Login/Login.jsx
+++ login-page/src/Components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+
 export class LoginForm extends Component {
   render() {
       return(
